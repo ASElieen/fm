61eaa02290d6e40c31ad80ac77e78201074c2fdd
username <790768275@qq.com>
2022-12-22 19:10:22 +0800

    feat:添加选项可以控制popover气泡位置

--- src/libs/Popover/Popover.vue
+++ src/libs/Popover/Popover.vue
@@ -1,12 +1,13 @@
 <template>
-    <div class="relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <div class="relative" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" ref="referenceTarget">
         <div>
             <!--具名插槽 用于触发弹层-->
             <slot name="reference" />
         </div>
 
         <Transition name="popover">
-            <div v-show="isVisible" class="absolute p-1 z-20 bg-white border rounded-md">
+            <div v-show="isVisible" class="absolute p-1 z-20 bg-white border rounded-md" ref="contentTarget"
+                :style="contentStyle">
                 <!--匿名插槽 弹层-->
                 <slot />
             </div>
@@ -30,7 +31,7 @@ const placementEnum = [
 </script>
 
 <script setup>
-import { ref } from "vue"
+import { ref, watch } from "vue"
 const props = defineProps({
     placement: {
         type: String,
@@ -54,6 +55,60 @@ const onMouseEnter = () => {
 const onMouseLeave = () => {
     isVisible.value = false
 }
+
+//计算元素尺寸 控制气泡相对位置
+const referenceTarget = ref(null)
+const contentTarget = ref(null)
+
+const useElementSize = (target) => {
+    if (!target) return
+    return {
+        width: target.offsetWidth,
+        height: target.offsetHeight
+    }
+}
+
+//弹层位置计算
+const contentStyle = ref({
+    top: 0,
+    left: 0
+})
+
+//展示时计算气泡位置
+watch(isVisible, (val) => {
+    if (!val) return
+    nextTick(() => {
+        switch (props.placement) {
+            // 左上
+            case PROP_TOP_LEFT:
+                contentStyle.value.top = 0
+                contentStyle.value.left =
+                    -useElementSize(contentTarget.value).width + 'px'
+                break
+            // 右上
+            case PROP_TOP_RIGHT:
+                contentStyle.value.top = 0
+                contentStyle.value.left =
+                    useElementSize(referenceTarget.value).width + 'px'
+                break
+            // 左下
+            case PROP_BOTTOM_LEFT:
+                contentStyle.value.top =
+                    useElementSize(referenceTarget.value).height + 'px'
+                contentStyle.value.left =
+                    -useElementSize(contentTarget.value).width + 'px'
+                break
+            // 右下
+            case PROP_BOTTOM_RIGHT:
+                contentStyle.value.top =
+                    useElementSize(referenceTarget.value).height + 'px'
+                contentStyle.value.left =
+                    useElementSize(referenceTarget.value).width + 'px'
+                break
+
+        }
+    })
+})
 </script>
 
 <style lang="scss" scoped>
