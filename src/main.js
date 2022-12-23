b0bfa9222b06d70102fda5a6ff99a2762468d515
username <790768275@qq.com>
2022-12-23 16:34:24 +0800

    chore:初始化vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/routerConfig'
+import store from '@/store/vuex'
 import '../style.css'
 import { useRem } from '@/utils/utils'
 import libs from '@/libs/libsConfig'
 import 'virtual:svg-icons-register'
 
 useRem()
-createApp(App).use(router).use(libs).mount('#app')
+createApp(App).use(router).use(libs).use(store).mount('#app')
