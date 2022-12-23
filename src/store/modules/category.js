ccfe6a0c6ad93ec4597060e9384d49eaf4cf88fe
username <790768275@qq.com>
2022-12-23 20:56:36 +0800

    refactor:修改state中category初始值，首次刷新不会闪烁

--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -1,12 +1,13 @@
 //navigationBar中的category数据
-import { ALL_CATEGORY_ITEM } from '@/constants/constants'
+import { CATEGORY_NORMAL_DATA } from '@/constants/constants'
 import { getCategory } from '@/api/api'
 export default {
     //独立作用域
     namespaced: true,
     //等价于()=>{ return {} }
     state: () => ({
-        categorys: [ALL_CATEGORY_ITEM]
+        //用每次获得的数据作为下一次初始化数据 解决刷新时PCNav组件闪烁的问题
+        categorys: [CATEGORY_NORMAL_DATA]
     }),
     mutations: {
         //给category赋值的唯一参数
