24979cd83b1a940b27790d95e4fbbb707d6220c8
username <790768275@qq.com>
2022-12-27 09:51:04 +0800

    feat使用window.matchMedia完成跟随系统功能

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,12 +2,23 @@ import { computed, watch } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 import { PC_DEVICE_WIDTH } from '../constants/constants'
 import store from '@/store/vuex'
-import { THEME_DARK, THEME_LIGHT } from '@/constants/constants'
+import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants/constants'
 
 const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
     return width.value < PC_DEVICE_WIDTH
 })
+
+//监听系统主题变更
+let matchMedia
+const watchSystemThemeChange = () => {
+    if (matchMedia) return
+    matchMedia = window.matchMedia('(prefers-color-scheme:dark)')
+    //监听主题变化
+    matchMedia.onchange = () => {
+        changeTheme(THEME_SYSTEM)
+    }
+}
 
 export const useRem = () => {
     const MAX_FONT_SIZE = 40
@@ -20,21 +31,27 @@ export const useRem = () => {
     })
 }
 
+const changeTheme = (val) => {
+    let themeClass = ''
+    switch (val) {
+        case THEME_LIGHT:
+            themeClass = 'light'
+            break
+        case THEME_DARK:
+            themeClass = 'dark'
+            break
+        case THEME_SYSTEM:
+            watchSystemThemeChange()
+            themeClass = matchMedia.matches ? 'dark' : 'light'
+            break
+    }
+
+    document.querySelector('html').className = themeClass
+}
+
 //初始化主题
 export const useTheme = () => {
-    watch(() => store.getters.themeType, (val) => {
-        let themeClass = ''
-        switch (val) {
-            case THEME_LIGHT:
-                themeClass = 'light'
-                break
-            case THEME_DARK:
-                themeClass = 'dark'
-                break
-        }
-
-        document.querySelector('html').className = themeClass
-    }, {
+    watch(() => store.getters.themeType, changeTheme, {
         immediate: true
     })
 }
