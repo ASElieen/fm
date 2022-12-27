71e65660363a0ff923935b3629f386588c350c59
username <790768275@qq.com>
2022-12-27 09:22:29 +0800

    feat:添加主题修改和本地缓存功能

--- src/views/layout/comps/Header/HeaderComps/HeaderTheme.vue
+++ src/views/layout/comps/Header/HeaderComps/HeaderTheme.vue
@@ -2,14 +2,14 @@
     <PopoverVue>
         <!--触发区-->
         <template #reference>
-            <SvgIconVue name="theme-light"
+            <SvgIconVue :name="iconName"
                 class="w-4 h-4 p-1 cursor-pointer rounded-sm duration-200 outline-none hover:bg-zinc-100/60 dark:hover:bg-zinc-900"
                 fillClass="fill-zinc-900 dark:fill-zinc-300" />
         </template>
         <!--展示区-->
         <div class="w-[140px] overflow-hidden">
             <div class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60 dark:hover:bg-zinc-800"
-                v-for="item in themeArr" :key="item.id">
+                v-for="item in themeArr" :key="item.id" @click="onItemClick(item)">
                 <SvgIconVue :name="item.icon" class="w-1.5 h-1.5 mr-1" fillClass="fill-zinc-900 dark:fill-zinc-300" />
                 <span class="text-zinc-800 dark:text-zinc-300 text-sm">{{ item.name }}</span>
             </div>
@@ -41,10 +41,25 @@ const themeArr = [
 </script>
 
 <script setup>
-import { } from "vue"
+import { computed } from "vue"
 import PopoverVue from "@/libs/Popover/Popover.vue";
 import SvgIconVue from "@/libs/SvgIcon/SvgIcon.vue";
 import { THEME_DARK, THEME_LIGHT, THEME_SYSTEM } from '@/constants/constants'
+import { useStore } from 'vuex';
+const store = useStore()
+
+//主题切换事件
+const onItemClick = (theme) => {
+    store.commit('theme/changeThemeType', theme.type)
+}
+
+//展示图标控制
+const iconName = computed(() => {
+    const findname = themeArr.find(item => {
+        return item.type === store.getters.themeType
+    })
+    return findname?.icon
+})
 </script>
 
 <style lang="scss" scoped>
