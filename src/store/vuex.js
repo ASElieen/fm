3ffb1d7d3be01bf233eddf06900dc18d9bc5d138
username <790768275@qq.com>
2022-12-27 09:08:00 +0800

    feat:vuex添加theme主题初始状态

--- src/store/vuex.js
+++ src/store/vuex.js
@@ -2,15 +2,17 @@ import { createStore } from 'vuex'
 import { createVuexPersistedState } from "vue-persistedstate"
 import getters from './getters'
 import category from './modules/category'
+import theme from './modules/theme'
 
 const store = createStore({
     getters,
     modules: {
-        category
+        category,
+        theme
     },
     plugins: [
         createVuexPersistedState({
-            key: 'category',
+            key: 'vuex',
             storage: window.localStorage
         })
     ]
